8fe54dfbb0627521f7eb88635b2c6a042e69ba2e
Michael Ha <77702012+Guacamoley@users.noreply.github.com>
2023-10-05 23:31:54 -0500

    Create UI for the RPS game

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-const readline = require("readline");
+// Get references to HTML elements
+const buttons = document.querySelectorAll("input");
+const resultDisplay = document.getElementById("result");
+const scoreDisplay = document.getElementById("score");
+
+// Initialize scores
+let scores = { player: 0, computer: 0 };
 
 function getComputerChoice() {
   const choices = ["Rock", "Paper", "Scissors"];
@@ -7,79 +13,80 @@ function getComputerChoice() {
   return randomChoice;
 }
 
+// Function to update the score display
+function updateScore() {
+  scoreDisplay.textContent = `Player: ${scores.player}, Computer: ${scores.computer}`;
+}
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    playRound(button.value, getComputerChoice(), scores);
+    updateScore();
+  });
+});
+
 function playRound(playerSelection, computerSelection, scores) {
   // Change output to lower case for easier comparison
   let lowercasePlayer = playerSelection.toLowerCase();
   let lowercaseComputer = computerSelection.toLowerCase();
 
   if (lowercasePlayer === lowercaseComputer) {
-    let message = "It's a draw! You both chose " + lowercasePlayer;
-    console.log(message);
+    resultDisplay.textContent =
+      "It's a draw! You both chose " + lowercasePlayer;
   } else if (
     (lowercasePlayer === "rock" && lowercaseComputer === "paper") ||
     (lowercasePlayer === "scissors" && lowercaseComputer === "rock") ||
     (lowercasePlayer === "paper" && lowercaseComputer === "scissors")
   ) {
-    let message =
+    resultDisplay.textContent =
       "You lose! " + lowercaseComputer + " beats " + lowercasePlayer;
-    console.log(message);
     scores.computer++;
   } else {
-    let message = "You win! " + lowercasePlayer + " beats " + lowercaseComputer;
-    console.log(message);
+    resultDisplay.textContent =
+      "You win! " + lowercasePlayer + " beats " + lowercaseComputer;
     scores.player++;
   }
-}
 
-function outputScores(scores) {
-  console.log("Game over! Final score: ");
-  console.log("Player: " + scores.player);
-  console.log("Computer: " + scores.computer);
-  if (scores.player === scores.computer) {
-    console.log("Its a draw!");
-  } else if (scores.player > scores.computer) {
-    console.log("Player wins!");
-  } else {
-    console.log("Computer wins!");
+  // Check for a winner (first to 5 points)
+  if (scores.player >= 5) {
+    resultDisplay.textContent = "Player wins the game!";
+    buttons.forEach((button) => {
+      button.disabled = true;
+    });
+  } else if (scores.computer >= 5) {
+    resultDisplay.textContent = "Computer wins the game!";
+    buttons.forEach((button) => {
+      button.disabled = true;
+    });
   }
 }
 
-function game() {
-  let round = 0;
-  const scores = { player: 0, computer: 0 };
+// function game() {
+//   const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout,
+//   });
 
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
+//   function playNextRound() {
+//     rl.question(
+//       "Please pick between Rock, Paper, or Scissors: ",
+//       (playerResponse) => {
+//         playerResponse = playerResponse.toLowerCase();
+//         if (["rock", "paper", "scissors"].includes(playerResponse)) {
+//           const computerChoice = getComputerChoice();
+//           playRound(playerResponse, computerChoice, scores);
+//           console.log("Player: " + scores.player);
+//           console.log("Computer: " + scores.computer);
+//           playNextRound(); // Continue playing without the round limit
+//         } else {
+//           console.log("Invalid choice. Please pick Rock, Paper, or Scissors.");
+//           playNextRound();
+//         }
+//       }
+//     );
+//   }
 
-  function playNextRound() {
-    rl.question(
-      "Please pick between Rock, Paper, or Scissors: ",
-      (playerResponse) => {
-        playerResponse = playerResponse.toLowerCase();
-        if (["rock", "paper", "scissors"].includes(playerResponse)) {
-          const computerChoice = getComputerChoice();
-          playRound(playerResponse, computerChoice, scores);
-          round++;
+//   playNextRound();
+// }
 
-          if (round < 5) {
-            playNextRound();
-          } else {
-            rl.close();
-            console.log("Game over! Final score:");
-            console.log("Player: " + scores.player);
-            console.log("Computer: " + scores.computer);
-          }
-        } else {
-          console.log("Invalid choice. Please pick Rock, Paper, or Scissors.");
-          playNextRound();
-        }
-      }
-    );
-  }
-
-  playNextRound();
-}
-
-game();
+// game();
